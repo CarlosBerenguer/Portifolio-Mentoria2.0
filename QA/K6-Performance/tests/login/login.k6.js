6c9fc6789ccaf09cd9ed3060ef236137a1010057
CarlosBerenguer <carloshfb03@gmail.com>
2025-08-22 19:40:05 -0300

    add stages e VUs para execuçãodo teste

--- QA/K6-Performance/tests/login/login.k6.js
+++ QA/K6-Performance/tests/login/login.k6.js
@@ -2,8 +2,18 @@ import http from 'k6/http'
 import { check, sleep } from 'k6'
 
 export const options = {
-    iterations: 10
-    // vus: 10,
+    //iterations: 10,
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95) < 3000'],
+    },
+
+    stages: [
+        { duration: '10s', target: 10 }, // 10 users requesting for 10 seconds
+        // { duration: '1m30s', target: 30 }, // 30 users requesting for 1m30sec
+        // { duration: '10s', target: 0 },
+    ],
+    //vus: 10,
     // duration: '30s',
 }
 
@@ -16,7 +26,7 @@ export default function () {
     const params = {
         headers: {
             'Content-Type': 'application/json',
-        },
+        }      
     }
     
     const response = http.post(url, JSON.stringify(payload), params)
